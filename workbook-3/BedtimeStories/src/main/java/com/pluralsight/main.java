5d20ab7db5027fb3e85120ab682874373abdd990
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-08 12:38:31 -0400

    PayrollCalculator

--- workbook-3/BedtimeStories/src/main/java/com/pluralsight/main.java
+++ workbook-3/BedtimeStories/src/main/java/com/pluralsight/main.java
@@ -6,44 +6,23 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in); //input from the user
         System.out.println("Please enter a book you would like to read: \n");
-        String pickStory = myScanner.nextLine().toLowerCase();
-//        myScanner.nextLine();
-
+        String fileName = myScanner.nextLine().toLowerCase(); //user’s input is captured
 
         try {
-            FileInputStream goldilocksFile = new FileInputStream("goldilocks.txt");
-            FileInputStream hanselFile = new FileInputStream( "hansel_and_gretel.txt");
-            FileInputStream maryFile = new FileInputStream("mary_had_a_little_lamb.txt");
-
-            Scanner goldilockScanner = new Scanner(goldilocksFile);
-            Scanner hanselScanner = new Scanner(hanselFile);
-            Scanner maryScanner = new Scanner(maryFile);
-
-            String goldiLine;
-            String hanselLine;
-            String maryLine;
-
-            while (goldilockScanner.hasNextLine()){
-                goldiLine = goldilockScanner.nextLine();
-                System.out.println(goldiLine);
+            FileInputStream fis = new FileInputStream(fileName); //open the file specified by the user
+            Scanner fileScanner = new Scanner(fis); //created to read the content of the file specified in fileName
+            int lineNumber = 1; //This will be used to print the line numbers next to each line of the file content starting with 1
+            while (fileScanner.hasNextLine()){ //keeps running as long as the file has more lines to read
+                String line = fileScanner.nextLine();
+                System.out.printf("%d. %s%n", lineNumber, line); //prints the current lineNumber and the content of the current line ' %d -integer, %s- string, %n-new line
+                lineNumber ++; //this increments the lineNumber variable by 1
             }
-            while (hanselScanner.hasNextLine()){
-                hanselLine = hanselScanner.nextLine();
-                System.out.println(hanselLine);
-            }
-            while (maryScanner.hasNextLine()){
-                maryLine = maryScanner.nextLine();
-                System.out.println(maryLine);
-            }
-            goldilockScanner.close();
-            hanselScanner.close();
-            maryScanner.close();
-
+            fileScanner.close();
 
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("File not found" + fileName);
         }
 
     }
---
+++ workbook-3/PayrollCalculator/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        String fileName = "employees.csv"; //the program will read this file
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)); //read the file line by line
+            String line; //store each line of the file as it is read
+
+            while((line = bufferedReader.readLine()) != null){ //if it is NOT null - continues
+                String [] tokens = line.split("\\|"); //split method by |
+                int id = Integer.parseInt(tokens[0]); //converted from a string to an integer
+                String name = tokens[1]; //split line (tokens[1]) is assigned to the name variable
+                double hoursWorked = Double.parseDouble(tokens[2]); // third part of the split line (tokens[2]) is converted to a double
+                double payRate = Double.parseDouble(tokens[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate); // object with usage of constructor
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            System.out.println("An error has occurred");
+        }
+    }
+}
