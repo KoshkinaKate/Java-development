23cd0471506b48626703c693eaed6bce82042c3e
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-23 13:16:40 -0400

    finished testing methods

--- workbook-4/HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
+++ workbook-4/HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
@@ -14,15 +14,21 @@ class EmployeeTest {
         kate.punchIn(checkInTime);
 
         //Assert
-        assertEquals(checkInTime,kate.getRegularHours());
+        assertEquals(checkInTime,kate.getStartTime());
     }
 
     @Test
     public void punchOut_GettingCheckOutTime_ReceivingTotalHoursOfWork(){
         //Arrange
+        Employee kate = new Employee(1,"Kate","development",150 );
+        double checkInTime = 9.0;
+        kate.punchIn(checkInTime);
         //Act
+        kate.punchOut(17.5);
+
         //Assert
-        
+        assertEquals(8.5, kate.getHoursWorked());
+
     }
 
 }
