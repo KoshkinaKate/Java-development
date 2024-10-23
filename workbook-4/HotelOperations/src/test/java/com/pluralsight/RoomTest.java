f33b25ae0e2f68af6e19756b5e517d88fefa5c3c
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-23 11:31:27 -0400

    test for checkOut and CleanRoom

--- workbook-4/HotelOperations/src/test/java/com/pluralsight/RoomTest.java
+++ workbook-4/HotelOperations/src/test/java/com/pluralsight/RoomTest.java
@@ -13,11 +13,36 @@ class RoomTest {
     // class  object       constructor
 
         //Act
-        kingRoom.checkIn();
+        kingRoom.checkIn(); //The checkIn() method is called on the kingRoom object.
+
 
         //Assert
         assertTrue(kingRoom.isDirty());
         assertTrue(kingRoom.isOccupied());
+    }
+
+    @Test
+    public void checkOut_RoomCondition_ShouldBeAvailable(){
+        //Arrange
+        Room kingRoom = new Room(2,139);
+
+        //Act
+        kingRoom.checkout();
+
+        //Assert
+        assertFalse(kingRoom.isOccupied());
+
+    }
+
+    @Test
+    public void cleanRoom_RoomCondition_CleanRoom(){
+        //Arrange
+        Room kingRoom = new Room(1,139);
+        //Act
+        kingRoom.cleanRoom();
+        //Assert
+        assertFalse(kingRoom.isDirty());
+
     }
 
 }
